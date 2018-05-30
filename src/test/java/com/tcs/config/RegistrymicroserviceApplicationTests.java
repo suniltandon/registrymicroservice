01883b640c4c3a48770d443655da63c25cc32552
suniltandon <skumar.tandon@gmail.com>
2018-05-30 15:14:53 -0500

    Initial commit registrymicroservice

---
+++ src/test/java/com/tcs/config/RegistrymicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tcs.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistrymicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
